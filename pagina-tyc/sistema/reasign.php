0ff5c3468041a6f59e90702869a3754aacc3899a
rubenqc <rwquispec@uni.pe>
2018-11-25 18:46:51 -0500

    Comienzo de mi aventura en php

--- pagina-tyc/sistema/reasign.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	rename("../img/bg/bgold.jpg", "../img/bg/yolo.jpg");
-	rename("../img/bg/bg.jpg", "../img/bg/bgold.jpg");
-	rename("../img/bg/yolo.jpg", "../img/bg/bg.jpg");
-
-	echo "La reasignación del fondo se realizó con éxito";
-
-?>
--- pagina-tyc/sistema/slides/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(!empty($_POST)){
-        foreach ($_POST as $deler) {
-            unlink('../../img/slides/'.$deler);
-        }
-    }
-
-    header("Location: ../slides",TRUE,303);
-?>
